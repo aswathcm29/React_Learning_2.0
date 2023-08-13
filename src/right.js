08fe9799fbae6416933ebbf33e1e5858eadce1dc
muruga21 <murugaperumal21062004@gmail.com>
2023-08-14 03:17:40 +0530

    added textarea

--- src/right.js
+++ src/right.js
@@ -1,12 +1,14 @@
 import './right.css'
 import Righttop from './righttop.js'
-import RightChatArea from './righttop.js'
+import Rightchatarea from './righttextarea.js'
+import Textview from './textview.js'
 
 function Right(){
     return(
         <div className="right">
             <Righttop/>
-            {/* <RightChatArea/> */}
+            <Textview/>
+            <Rightchatarea/>
         </div>
     );
 }
--- src/righttextarea.js
+++ src/righttextarea.js
@@ -1,1 +1,14 @@
 import './right.css'
+
+function Rightchatarea(){
+    return(
+        <div className='Textarea'>
+            <input type="textarea" className='inputarea'></input>
+            <button className='send'>
+                send
+            </button>
+        </div>
+    );
+}
+
+export default Rightchatarea;
