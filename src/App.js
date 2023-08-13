9e20387e388d6f356e3b5d0fa200383ed8d7de90
muruga21 <murugaperumal21062004@gmail.com>
2023-08-14 01:34:30 +0530

    tryin to fix righttop

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Left from './left.js';
+import Right from './right';
 
 function App() {
   return (
     <div class="container">
        <Left/>
+       <Right/>
     </div>
   );
 }
