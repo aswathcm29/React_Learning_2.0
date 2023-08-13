b28170cdf700b8660444f416d64f5d8b80b6acf3
aswathcm2904 <aswath.cm2022cse@sece.ac.in>
2023-08-14 00:57:01 +0530

    fixed right.js

--- src/right.js
+++ src/right.js
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-function Right(){
-    return(
-        
-    );
-}
-
-=======
 import './right.css'
 import RightTop from './righttop.js'
 import RightChatArea from './righttop.js'
@@ -18,5 +10,4 @@ Right = () =>{
         </div>
     );
 }
->>>>>>> e38789cabfe8447c0f9c409920baee47c4cfc346
 export default Right;
